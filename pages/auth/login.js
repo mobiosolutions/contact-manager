02b9bf5d8c70af399adc5ea671a989b25b64160a
kirandbmobio <kiranb.mobio@gmail.com>
2020-01-20 11:32:34 +0530

    login-test-case - update login test cases

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -10,161 +10,143 @@ import Lock from "@material-ui/icons/Lock";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Router from "next/router";
 
-import {signinUser} from "../../lib/auth";
+import { signinUser } from "../../lib/auth";
 
 class Login extends React.Component {
-    state = {
-        email: '',
-        password: '',
-        error: '',
-        openError: false,
-        isLoading: false
-    };
+  state = {
+    email: "",
+    password: "",
+    error: "",
+    openError: false,
+    isLoading: false
+  };
 
-    handleClose = () => this.setState({
-        openError: false
-
+  handleClose = () =>
+    this.setState({
+      openError: false
     });
 
-    handleChange = event => {
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-        this.setState({ [event.target.name]: event.target.value })
+  handleSubmit = event => {
+    const { email, password } = this.state;
+    event.preventDefault();
+    const user = { email, password };
+    this.setState({ isLoading: true, error: "" });
 
-    }
+    signinUser(user)
+      .then(() => {
+        Router.push("/");
+      })
+      .catch(this.showError);
 
-    handleSubmit = event => {
-        const {email , password} = this.state;
-        event.preventDefault();
-        const user = { email, password };
-        this.setState({ isLoading:true , error: ''});
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-        signinUser(user)
-            .then(() => {
-                Router.push('/')
-            }).catch(this.showError);
+  showError = err => {
+    console.log(err);
+    const error = (err.response && err.response.data.message) || err.message;
+    this.setState({ error, openError: true, isLoading: false });
+  };
 
-        this.setState({ [event.target.name]: event.target.value })
+  render() {
+    const { classes } = this.props;
+    const { error, openError, isLoading } = this.state;
 
-    }
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <Lock />
+          </Avatar>
 
-    showError = err => {
-       console.log(err);
-        const error = err.response && err.response.data.message || err.message;
-        this.setState({ error, openError:true, isLoading:false });
+          <Typography variant="h5" component="h1">
+            Sign In
+          </Typography>
+          <form onSubmit={this.handleSubmit} className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email"> Email </InputLabel>
+              <Input name="email" type="email" onChange={this.handleChange} />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password"> Password </InputLabel>
+              <Input
+                name="password"
+                type="password"
+                onChange={this.handleChange}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              disabled={isLoading}
+              className={classes.submit}
+            >
+              {isLoading ? "Signing in..." : "Sign in"}
+            </Button>
+          </form>
 
+          {/*{ Error snackbar}*/}
 
-    }
-
-    render() {
-        const {classes} = this.props;
-        const { error , openError , isLoading } = this.state;
-
-
-        return (
-            <div className={classes.root}>
-                <Paper className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <Lock/>
-
-                    </Avatar>
-
-                    <Typography variant="h5" component="h1">
-
-                        Sign In
-
-                    </Typography>
-                    <form onSubmit={this.handleSubmit} className={classes.form}>
-
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="email"> Email </InputLabel>
-                            <Input
-                                name="email"
-                                type="email"
-                                onChange = { this.handleChange }
-
-                            />
-
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="password"> Password </InputLabel>
-                            <Input
-                                name="password"
-                                type="password"
-                                onChange = { this.handleChange }
-
-                            />
-
-                        </FormControl>
-                        <Button
-                            type="submit" fullWidth
-                            variant="contained"
-                            color="primary"
-                            disabled={isLoading}
-                            className={classes.submit}
-                        >
-                            { isLoading ? "Signing in..." : "Sign in"}
-                        </Button>
-
-                    </form>
-
-                    {/*{ Error snackbar}*/}
-
-                    { error &&    <Snackbar
-                        anchorOrigin={{
-                            vertical : 'bottom',
-                            horizontal : 'right'
-                        }}
-                        open={openError}
-                        onClose={this.handleClose}
-                        autoHideDuration={6000}
-                        message = {<span className={classes.snack}> { error } </span> }
-
-                    />}
-
-                </Paper>
-
-            </div>
-        )
-    }
+          {error && (
+            <Snackbar
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right"
+              }}
+              open={openError}
+              onClose={this.handleClose}
+              autoHideDuration={6000}
+              message={<span className={classes.snack}> {error} </span>}
+            />
+          )}
+        </Paper>
+      </div>
+    );
+  }
 }
 
 const styles = theme => ({
-    root: {
-        width: "auto",
-        display: "block",
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up("md")]: {
-            width: 400,
-            marginLeft: "auto",
-            marginRight: "auto"
-        }
-    },
-    paper: {
-        marginTop: theme.spacing.unit * 8,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: theme.spacing.unit * 2
-    },
-    avatar: {
-        margin: theme.spacing.unit,
-        backgroundColor: theme.palette.secondary.main
-    },
-    form: {
-        width: "100%",
-        marginTop: theme.spacing.unit
-    },
-    submit: {
-        marginTop: theme.spacing.unit * 2,
-        backgroundColor: '#1976d2',
-        "&:hover": {
-            background: "#2176d2"
-        }
-    },
-    snack: {
-        color: theme.palette.secondary.light
+  root: {
+    width: "auto",
+    display: "block",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up("md")]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
     }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: theme.spacing.unit * 2
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 2,
+    backgroundColor: "#1976d2",
+    "&:hover": {
+      background: "#2176d2"
+    }
+  },
+  snack: {
+    color: theme.palette.secondary.light
+  }
 });
 
 export default withStyles(styles)(Login);
--- test/pages/auth/login.spec.js
+++ test/pages/auth/login.spec.js
@@ -1,36 +1,94 @@
-import React from 'react'
-import { mount } from '../../testkit/dom-test'
-import {shallow, configure} from 'enzyme'
-import Login from '../../../pages/auth/login'
-import Adapter from 'enzyme-adapter-react-16'
+import React from "react";
+import { mount } from "../../testkit/dom-test";
+import { shallow, configure } from "enzyme";
+import Login from "../../../pages/auth/login";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() });
 
-describe('Test case for testing login',() =>{
-    let wrapper;
-    test('username check',()=>
-    {
-        wrapper = shallow(<Login/>);
-        wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'vipiny@gmail.com'}});
-        expect(wrapper.state('email')).toEqual('vipiny@gmail.com');
-    })
-    it('password check',()=>{
-        wrapper = shallow(<Login/>);
-        wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: '123456'}});
-        expect(wrapper.state('password')).toEqual('123456');
-    })
-    it('login check with right data',()=>{
-        wrapper = shallow(<Login/>);
-        wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'vipiny@gmail.com'}});
-        wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: '123456'}});
-        wrapper.find('button').simulate('click');
-        expect(wrapper.state('isLogined')).toBe(true);
-    })
-    it('login check with wrong data',()=>{
-        wrapper = shallow(<Login/>);
-        wrapper.find('input[type="text"]').simulate('change', {target: {name: 'username', value: 'vipiny@gmail.com'}});
-        wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: '123456'}});
-        wrapper.find('button').simulate('click');
-        expect(wrapper.state('isLogined')).toBe(false);
-    })
-})
+describe("Test case for testing login", () => {
+  let wrapper;
+  /* if email value is undefined and attribute should be name */
+  it("should check email", () => {
+    wrapper = mount(<Login />);
+    let email = wrapper.find('Input[type="email"]');
+    expect(email.attr("name")).toBe("email");
+    expect(email.val()).toBe(undefined);
+    expect(email.length).toBe(1);
+  });
+
+  /* if password value is undefined and attribute should be name */
+  it("should check password", () => {
+    wrapper = mount(<Login />);
+    let password = wrapper.find('Input[type="password"]');
+    expect(password.attr("name")).toBe("password");
+    expect(password.val()).toBe(undefined);
+    expect(password.length).toBe(1);
+  });
+
+  /* if button value is undefined and color is primary */
+  it("should check login button", () => {
+    wrapper = mount(<Login />);
+    let button = wrapper.find("Button");
+    expect(button.attr("color")).toBe("primary");
+    expect(button.val()).toBe(undefined);
+    expect(button.length).toBe(1);
+  });
+
+  /* if email has some value it failed */
+  it("should check failed email", () => {
+    wrapper = mount(<Login />);
+    let email = wrapper.find('Input[type="email"]');
+    expect(email.attr("name")).toBe("email");
+    expect(email.val()).toBe("kiran@gmail.com");
+    expect(email.length).toBe(1);
+  });
+
+  /* if password has some value it failed */
+  it("should check failed password", () => {
+    wrapper = mount(<Login />);
+    let password = wrapper.find('Input[type="password"]');
+    expect(password.attr("name")).toBe("password");
+    expect(password.val()).toBe("1234");
+    expect(password.length).toBe(1);
+  });
+
+  /* if button has some value it failed */
+  it("should check login button", () => {
+    wrapper = mount(<Login />);
+    let button = wrapper.find("Button");
+    expect(button.attr("color")).toBe("primary");
+    expect(button.val()).toBe("submit");
+    expect(button.length).toBe(1);
+  });
+
+  //   it("password check", () => {
+  //     wrapper = shallow(<Login />);
+  //     wrapper
+  //       .find('input[type="password"]')
+  //       .simulate("change", { target: { name: "password", value: "123456" } });
+  //     expect(wrapper.state("password")).toEqual("123456");
+  //   });
+  //   it("login check with right data", () => {
+  //     wrapper = shallow(<Login />);
+  //     wrapper.find('input[type="text"]').simulate("change", {
+  //       target: { name: "email", value: "vipiny@gmail.com" }
+  //     });
+  //     wrapper
+  //       .find('input[type="password"]')
+  //       .simulate("change", { target: { name: "password", value: "123456" } });
+  //     wrapper.find("button").simulate("click");
+  //     expect(wrapper.state("isLogined")).toBe(true);
+  //   });
+  //   it("login check with wrong data", () => {
+  //     wrapper = shallow(<Login />);
+  //     wrapper.find('input[type="text"]').simulate("change", {
+  //       target: { name: "username", value: "vipiny@gmail.com" }
+  //     });
+  //     wrapper
+  //       .find('input[type="password"]')
+  //       .simulate("change", { target: { name: "password", value: "123456" } });
+  //     wrapper.find("button").simulate("click");
+  //     expect(wrapper.state("isLogined")).toBe(false);
+  //   });
+});
